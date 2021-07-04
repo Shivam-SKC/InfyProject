39fa14041e5ffb81418f352e6b3a3654cb183f2a
mafiadeveloper <choubeyshivam555@gmail.com>
2021-07-04 21:07:46 +0530

    modified methods

--- Employee/src/main/java/com/infy/service/EmployeeServiceImpl.java
+++ Employee/src/main/java/com/infy/service/EmployeeServiceImpl.java
@@ -1,9 +1,10 @@
 package com.infy.service;
 
-
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,51 +13,51 @@ import com.infy.entity.Employee;
 import com.infy.exception.EmployeeException;
 import com.infy.repository.EmployeeRepository;
 
-@Service(value="employeeService")
+@Service(value = "employeeService")
 @Transactional
-public class EmployeeServiceImpl implements EmployeeService{
-	
-	 @Autowired
-	 EmployeeRepository repo;
-	 
-	 
-	 //adding new employee
-	 @Override
-	 public Integer addEmployee(EmployeeDTO employeeDTO)throws EmployeeException{
-		 Employee e = new Employee();
-		 e.setName(employeeDTO.getName());
-		 e.setLevel(employeeDTO.getLevel());
-		 repo.save(e);
-		 return e.getEmployeeId();
-	 }
-	 
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	private EmployeeRepository repo;
+
+	// adding new employee
 	@Override
-	 public EmployeeDTO getEmployee(Integer empid) throws EmployeeException{
-		 Optional<Employee> op=repo.findById(empid);
-		 Employee e=op.orElseThrow(()->new EmployeeException("Employee not found"));
-		 EmployeeDTO eDTO=new EmployeeDTO();
-		 eDTO.setEmployeeId(e.getEmployeeId());
-		 eDTO.setName(e.getName());
-		 eDTO.setLevel(e.getLevel());
-		return eDTO;
-		 
-	 }
-	@Override
-	public void updateEmployee(Integer empId,EmployeeDTO employeeDTO)throws EmployeeException{
-		 Optional<Employee> op=repo.findById(empId);
-		 Employee e=op.orElseThrow(()->new EmployeeException("Employee not found"));
-		 e.setName(employeeDTO.getName());
-		 e.setEmployeeId(employeeDTO.getEmployeeId());
-		 e.setLevel(employeeDTO.getLevel());
-		 repo.save(e);
+	public Integer addEmployee(EmployeeDTO employeeDTO) throws EmployeeException {
+		Employee e = new Employee();
+		e.setName(employeeDTO.getName());
+		e.setLevel(employeeDTO.getLevel());
+		repo.save(e);
+		return e.getEmployeeId();
 	}
-	
+
 	@Override
-	public Integer deleteEmployee(Integer empid)throws EmployeeException{
-		Optional<Employee> op=repo.findById(empid);
-		 Employee e=op.orElseThrow(()->new EmployeeException("Employee not found"));
-		 repo.deleteById(e.getEmployeeId());
-		 return e.getEmployeeId();
+	public EmployeeDTO getEmployee(Integer empid) throws EmployeeException {
+		Optional<Employee> op = repo.findById(empid);
+		Employee e = op.orElseThrow(() -> new EmployeeException("Service.EMPLOYEE_NOT_FOUND"));
+		EmployeeDTO eDTO = new EmployeeDTO();
+		eDTO.setEmployeeId(e.getEmployeeId());
+		eDTO.setName(e.getName());
+		eDTO.setLevel(e.getLevel());
+		return eDTO;
+
+	}
+
+	@Override
+	public void updateEmployee(Integer empId, EmployeeDTO employeeDTO) throws EmployeeException {
+		Optional<Employee> op = repo.findById(empId);
+		Employee e = op.orElseThrow(() -> new EmployeeException("Service.EMPLOYEE_NOT_FOUND"));
+		e.setName(employeeDTO.getName());
+		e.setEmployeeId(employeeDTO.getEmployeeId());
+		e.setLevel(employeeDTO.getLevel());
+		repo.save(e);
+	}
+
+	@Override
+	public Integer deleteEmployee(Integer empid) throws EmployeeException {
+		Optional<Employee> op = repo.findById(empid);
+		Employee e = op.orElseThrow(() -> new EmployeeException("Service.EMPLOYEE_NOT_FOUND"));
+		repo.deleteById(e.getEmployeeId());
+		return e.getEmployeeId();
 	}
 
 }
