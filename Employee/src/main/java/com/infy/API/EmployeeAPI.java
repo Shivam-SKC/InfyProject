9b751be71525a7b731e6cfbca6738fd625738aee
mafiadeveloper <choubeyshivam555@gmail.com>
2021-07-04 21:09:20 +0530

    modified methods with validation and swagger

--- Employee/src/main/java/com/infy/API/EmployeeAPI.java
+++ Employee/src/main/java/com/infy/API/EmployeeAPI.java
@@ -1,8 +1,13 @@
 package com.infy.API;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,41 +18,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.infy.DTO.EmployeeDTO;
+import com.infy.entity.Employee;
 import com.infy.exception.EmployeeException;
 import com.infy.service.EmployeeService;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@CrossOrigin
+@Validated
 @RequestMapping(value="/Employee")
 public class EmployeeAPI {
 	@Autowired
 	private EmployeeService service;
+	
+	@Autowired
+	private Environment environment;
 
 	
-	@PostMapping("/addEmployee")
-	public ResponseEntity<String> addEmployee( @RequestBody EmployeeDTO employeeDTO) throws EmployeeException{
+	@PostMapping("/")
+	public ResponseEntity<String> addEmployee( @Valid @RequestBody EmployeeDTO employeeDTO) throws EmployeeException{
 		Integer empid=service.addEmployee(employeeDTO);
-		String Successmessage="The new employee added with "+empid;
+		String Successmessage=environment.getProperty("API.INSERT_SUCCESS")+empid;
 		return new ResponseEntity<>(Successmessage,HttpStatus.CREATED);
 		
 	}
 	
-	@GetMapping("/getEmployee/{employeeId}")
+	@GetMapping("/{employeeId}")
 	public ResponseEntity<EmployeeDTO> getEmployee(@PathVariable Integer employeeId)throws EmployeeException{
 		return new ResponseEntity<>(service.getEmployee(employeeId),HttpStatus.OK);
 	}
 	
-	@PutMapping("/updateEmployee/{employeeId}")
-	public ResponseEntity<String> updateEmployee(@PathVariable Integer employeeId,@RequestBody EmployeeDTO employeeDTO)throws EmployeeException{
+	@PutMapping("/{employeeId}")
+	public ResponseEntity<String> updateEmployee(@PathVariable Integer employeeId,@Valid @RequestBody EmployeeDTO employeeDTO)throws EmployeeException{
 		service.updateEmployee(employeeId,employeeDTO);
-		String SuccessMessage="Employee details is updated for empId"+employeeId;
+		String SuccessMessage=environment.getProperty("API.UPDATE_SUCCESS")+employeeId;
 		return new ResponseEntity<String>(SuccessMessage,HttpStatus.ACCEPTED);
 		
 	}
 	
-	@DeleteMapping("/deleteEmployee/{employeeId}")
+	@DeleteMapping("/{employeeId}")
+	@ApiOperation(value = "Deleting employee details", notes = "Give a employee id to delete the details of an employee", response = Employee.class)
 	public ResponseEntity<String> deleteEmployee( @PathVariable Integer employeeId)throws EmployeeException{
 		Integer empId=service.deleteEmployee(employeeId);
-		String SuccessMessage="The Employee is deleted with "+empId;
+		String SuccessMessage=environment.getProperty("API.DELETE_SUCCESS")+empId;
 		return new ResponseEntity<String>(SuccessMessage,HttpStatus.OK);
 	}
 	
